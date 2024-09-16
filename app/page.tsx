dc9f443fe6a00627485a1b3667758b322a1a15c9
Manuel Tati <manuel.tati@minfin.gov.ao>
2024-09-16 17:27:14 +0100

    ajustes na descrição

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          A MQ2MK é uma consultoria de <span className="font-semibold">arquitetura de soluções</span> focada em oferecer <br className="hidden sm:inline" /> soluções personalizadas e inovadoras para pequeno e médio empresas.
+        <h1 className="text-3xl font-semibold leading-tight tracking-tighter md:text-4xl">
+          A MQ2MK é uma consultoria de <span className="font-bold">arquitetura de soluções</span> focada em oferecer soluções personalizadas  <br className="hidden sm:inline" /> e inovadoras para pequeno e médio empresas.
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
           Com uma equipe altamente qualificada e experiência em diversas tecnologias, a MQ2MK ajuda seus clientes a transformar ideias em produtos digitais robustos e escaláveis.
