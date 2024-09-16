11a52d783f911fb88eee196d48c5193a24dc86b7
Manuel Tati <manuel.tati@minfin.gov.ao>
2024-09-16 11:01:35 +0100

    project init

---
+++ app/page.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link"
+
+import { buttonVariants } from "@/components/ui/button"
+import { siteConfig } from "@/config/site"
+
+export default function IndexPage() {
+  return (
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          A MQ2MK é uma consultoria de desenvolvimento de software focada em oferecer <br className="hidden sm:inline" /> soluções personalizadas e inovadoras para pequeno e médio empresas.
+        </h1>
+        <p className="max-w-[700px] text-lg text-muted-foreground">
+          Com uma equipe altamente qualificada e experiência em diversas tecnologias, a MQ2MK ajuda seus clientes a transformar ideias em produtos digitais robustos e escaláveis.
+        </p>
+      </div>
+      <div className="flex gap-4">
+        <Link
+          href={siteConfig.links.docs}
+          target="_blank"
+          rel="noreferrer"
+          className={buttonVariants()}
+        >
+          Documentation
+        </Link>
+        <Link
+          target="_blank"
+          rel="noreferrer"
+          href={siteConfig.links.github}
+          className={buttonVariants({ variant: "outline" })}
+        >
+          GitHub
+        </Link>
+      </div>
+    </section>
+  )
+}
